92df3d9e98484504e5e3030b7e917c6b6fa1cc29
Rubayedprodhan <rubayedprodhan550@gmail.com>
2024-09-18 00:45:59 +0600

    requirements.txt added

--- bank_management/settings.py
+++ bank_management/settings.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+import dj_database_url
+
 import environ
 env = environ.Env()
-import dj_database_url
+
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
